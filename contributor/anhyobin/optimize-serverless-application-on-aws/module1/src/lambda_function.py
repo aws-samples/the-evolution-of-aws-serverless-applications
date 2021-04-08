ce504729238b4dd13d3ef342e608ae678d444d2f
anhyobin <76983889+anhyobin@users.noreply.github.com>
2021-04-08 23:24:58 +0900

    Add files via upload

--- contributor/anhyobin/optimize-serverless-application-on-aws/module1/src/lambda_function.py
+++ contributor/anhyobin/optimize-serverless-application-on-aws/module1/src/lambda_function.py
@@ -1,1 +1,16 @@
+import json
+import boto3
 
+sns = boto3.client('sns')
+
+def lambda_handler(event, context):
+    response = sns.publish(
+        TopicArn = 'YOUR SNS TOPIC ARN',
+        Message = event['Records'][0]['s3']['object']['key'] + ' has been ' + event['Records'][0]['eventName'],
+        Subject = 'S3  Event',
+        )
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
